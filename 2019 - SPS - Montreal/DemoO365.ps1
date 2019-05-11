aff453f05c02f4ebe0bb35dc2abb1ec595e26ad1
Nik Charlebois <nicharl@microsoft.com>
2019-05-11 08:09:06 -0400

    SPS

---
+++ 2019 - SPS - Montreal/DemoO365.ps1
@@ -0,0 +1,34 @@
+﻿Configuration DemoO365
+{
+    Import-DSCResource -ModuleName Office365DSC
+    $GlobalAdmin = Get-Credential
+    Node localhost
+    {
+        TeamsTeam MySUperTeam
+        {
+            DisplayName = "Mon Super Team"
+            GlobalAdminAccount = $GlobalAdmin
+            Ensure = "Present"
+        }
+
+        TeamsChannel Keynote
+        {
+            DisplayName = "KeyNote"
+            TeamName = "Mon Super Team"
+            GlobalAdminAccount = $GlobalAdmin
+            Ensure = "Present"
+        }
+    }
+}
+
+$config = @{
+    AllNodes = @(
+        @{
+            NodeName = "localhost"
+            PSDscAllowPlainTextPassword = $true
+        }
+    )
+}
+
+DemoO365 -COnfigurationData $config
+Start-DSCConfiguration DemoO365 -Wait -Verbose -Force
