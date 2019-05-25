4be760dc487d2482fe016fbe97560bfae069225e
Nik Charlebois <nicharl@microsoft.com>
2019-05-25 08:56:22 -0700

    Update Deploy.ps1

--- 2019 - CollabSummit - Europe/Deploy.ps1
+++ 2019 - CollabSummit - Europe/Deploy.ps1
@@ -3,7 +3,11 @@
     [string]$TargetEnvironment
 )
 Write-Host "Creating Resource Group {CollabSummit$($TargetEnvironment)}..." -NoNewLine
-$rg = (New-AzureRmResourceGroup -Name "CollabSummit$($TargetEnvironment)" -Location WestUS).ResourceGroupName
+$rg = Get-AzureRMResourceGroup "CollabSummit$($TargetEnvironment)" -ErrorAction SilentlyContinue
+if ($null -eq $rg)
+{
+    $rg = (New-AzureRmResourceGroup -Name "CollabSummit$($TargetEnvironment)" -Location WestUS).ResourceGroupName
+}
 Write-Host "Done" -ForegroundColor Green
 
 $templateFile = "$(System.DefaultWorkingDirectory)/Demo/Demo/CollabSummit/SharePoint2019/azuredeploy.json"
