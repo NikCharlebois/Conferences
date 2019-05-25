073cfdb9e33938f15b5645aa3fafc2678b615c44
Nik Charlebois <nicharl@microsoft.com>
2019-05-25 08:16:29 -0700

    Release Pipe

--- 2019 - CollabSummit - Europe/Deploy.ps1
+++ 2019 - CollabSummit - Europe/Deploy.ps1
@@ -1,14 +1,15 @@
-﻿#Login-AzureRmAccount
-Select-AzureRmSubscription "Consommation interne Microsoft Azure"
-$rg1 = (New-AzureRmResourceGroup -Name TBD1 -Location WestUS).ResourceGroupName
+﻿param ( 
+    [parameter(Mandatory = $true)]
+    [string]$TargetEnvironment
+)
+$rg = (New-AzureRmResourceGroup -Name "CollabSummit$($TargetEnvironment)" -Location WestUS).ResourceGroupName
 
-$templateFile = "C:\GitHub\Conferences\2019 - CollabSummit - Europe\SharePoint2019\azuredeploy.json"
+$templateFile = ".\azuredeploy.json"
 $adminUserName = "lcladmin"
 $password = ConvertTo-SecureString "Pass@word!11" -AsPlainText -Force
-$dnsLabelPrefix = "myvm-$(Get-Random)"
-$windowsOsVersion = "2016-Datacenter"
+
 New-AzureRmResourceGroupDeployment -Verbose `
-    -ResourceGroupName $rg1 `
+    -ResourceGroupName $rg `
     -TemplateFile $templateFile `
     -adminUsername $adminUserName `
     -adminPassword $password
