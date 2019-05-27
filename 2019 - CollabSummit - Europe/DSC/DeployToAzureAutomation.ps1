788803699ba7fc2c85cf3c68a5dea40f251f2b78
Nik Charlebois <nicharl@microsoft.com>
2019-05-27 09:37:58 -0400

    Fixes

--- 2019 - CollabSummit - Europe/DSC/DeployToAzureAutomation.ps1
+++ 2019 - CollabSummit - Europe/DSC/DeployToAzureAutomation.ps1
@@ -2,7 +2,9 @@ param (
     [parameter(Mandatory = $true)]
     [string]$TargetEnvironment
 )
-$DSCScriptPath           = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "SharePoint2019.ps1"))
+$DSCScriptPath         = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "SharePoint2019.ps1"))
+$ConfigDataPath        = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "ConfigurationData-$($TargetEnvironment).psd1"))
+
 $AutomationAccountName = "CollabSummit"
 $ResourceGroupName     = "CS$TargetEnvironment"
 $WebFrontEndVM         = "SPWFE$ResourceGroupName"
@@ -13,7 +15,7 @@ Write-Output "Uploading the SharePoint Configuration into Azure Automation..."
 Import-AzureRmAutomationDscConfiguration -SourcePath $DSCScriptPath -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Published -Force
 
 Write-Output "Compiling the SharePoint Configuration within Azure Automation..."
-Start-AzureRmAutomationDscCompilationJob -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ConfigurationName "SharePoint2019"
+Start-AzureRmAutomationDscCompilationJob -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ConfigurationName "SharePoint2019" -ConfigurationData $ConfigDataPath
 
 #Write-Output "Registering the SQL Server with Azure Automation"
 #Register-AzureRmAutomationDscNode -AzureVMResourceGroup $ResourceGroupName -AzureVMName $SQLServerName -AzureVMLocation $ResourceGroupLocation -NodeConfigurationName "SQL.localhost" -ActionAfterReboot ContinueConfiguration -RebootNodeIfNeeded $true -AutomationAccountName $MasterAutomationAccountName -ResourceGroupName $MasterResourceGroupName
