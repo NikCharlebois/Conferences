32f4d457612df447314c3168c6e9a33b0e3789c9
Nik Charlebois <nicharl@microsoft.com>
2019-05-27 09:33:20 -0400

    Revamped Structure

---
+++ 2019 - CollabSummit - Europe/DSC/ConfigurationData-Dev.psd1
@@ -0,0 +1,20 @@
+@{
+    AllNodes = @(
+        @{
+            NodeName = "SPWFECSDev"
+            MinRole  = "WebFrontEnd"
+        },
+        @{
+            NodeName = "SPAPPCSDev"
+            MinRole  = "Application"
+        },
+        @{
+            NodeName = "SPSRCCSDev"
+            MinRole  = "Search"
+        }
+    )
+    Settings = @{
+        DomainName  = "contoso.com"
+        PrimaryADIP = "10.0.0.4"
+    }
+}
---
+++ 2019 - CollabSummit - Europe/DSC/DeployToAzureAutomation.ps1
@@ -0,0 +1,19 @@
+param ( 
+    [parameter(Mandatory = $true)]
+    [string]$TargetEnvironment
+)
+$DSCScriptPath           = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "SharePoint2019.ps1"))
+$AutomationAccountName = "CollabSummit"
+$ResourceGroupName     = "CS$TargetEnvironment"
+$WebFrontEndVM         = "SPWFE$ResourceGroupName"
+$ApplicationVM         = "SPAPP$ResourceGroupName"
+$SearchVM              = "SPSRC$ResourceGroupName"
+
+Write-Output "Uploading the SharePoint Configuration into Azure Automation..."
+Import-AzureRmAutomationDscConfiguration -SourcePath $DSCScriptPath -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -Published -Force
+
+Write-Output "Compiling the SharePoint Configuration within Azure Automation..."
+Start-AzureRmAutomationDscCompilationJob -ResourceGroupName $ResourceGroupName -AutomationAccountName $AutomationAccountName -ConfigurationName "SharePoint2019"
+
+#Write-Output "Registering the SQL Server with Azure Automation"
+#Register-AzureRmAutomationDscNode -AzureVMResourceGroup $ResourceGroupName -AzureVMName $SQLServerName -AzureVMLocation $ResourceGroupLocation -NodeConfigurationName "SQL.localhost" -ActionAfterReboot ContinueConfiguration -RebootNodeIfNeeded $true -AutomationAccountName $MasterAutomationAccountName -ResourceGroupName $MasterResourceGroupName
