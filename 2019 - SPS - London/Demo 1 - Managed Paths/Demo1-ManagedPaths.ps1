c9475ba3967dd60240657b1c5dd4eba408bb1e61
Nik Charlebois <nicharl@microsoft.com>
2019-06-01 16:34:53 +0100

    SPSLondon

---
+++ 2019 - SPS - London/Demo 1 - Managed Paths/Demo1-ManagedPaths.ps1
@@ -0,0 +1,49 @@
+﻿Configuration Demo1
+{
+    Import-DSCResource -ModuleName SharePointDSC -ModuleVersion 3.4.0.0
+    $FarmAdmin = Get-Credential -Username "contoso\sp_farm" -Message "Farm Admin Account"
+    Node localhost
+    {
+	SPWebApplication SPSLondon
+	{
+		Name                   = "SPSLondon"
+		Url                    = "https://SPSLondon.contoso.com"
+		Port                   = 443
+		ApplicationPool        = "SPSLondon"
+		ApplicationPoolAccount = $FarmAdmin.UserName
+		Ensure                 = "Present"
+		PSDscRunAsCredential   = $FarmAdmin
+	}
+        SPManagedPath VideosMP
+        {
+            WebAppUrl            = "https://SPSLondon.contoso.com"
+            RelativeUrl          = "Videos"
+            Explicit             = $false
+            HostHeader           = $false
+            Ensure               = "Present"
+            PSDSCRunAsCredential = $FarmAdmin
+        }
+
+        SPManagedPath ArchivesMP
+        {
+            WebAppUrl            = "https://SPSLondon.contoso.com"
+            RelativeUrl          = "Archives"
+            Explicit             = $false
+            HostHeader           = $false
+            Ensure               = "Present"
+            PSDSCRunAsCredential = $FarmAdmin
+        }
+    }
+}
+
+$ConfigData = @{
+    AllNodes = @(
+        @{
+            NodeName = "localhost"
+            PSDscAllowPlaintextPassword = $true
+            PSDscAllowDomainUser = $true
+        }
+    )
+}
+
+Demo1 -ConfigurationData $ConfigData
---
+++ 2019 - SPS - London/Demo 3 - Teams Deployment/Demo3-TeamsDeployment.ps1
@@ -0,0 +1,46 @@
+﻿Configuration Demo3
+{
+    Import-DscResource -ModuleName Office365DSC
+    $Creds = Get-Credential
+    Node localhost
+    {
+        TeamsTeam SPSLondon
+        {
+            DisplayName             = "SPS London"
+            AllowGiphy              = $true
+            AllowUserDeleteMessages = $true
+            GlobalAdminAccount      = $Creds
+            Ensure                  = "Present"
+        }
+
+        TeamsChannel UserFeedback
+        {
+            TeamName           = "SPS London"
+            DisplayName        = "Feedback"
+            Description        = "Channel to capture feedback"
+            GlobalAdminAccount = $Creds
+            DependsOn          = "[TeamsTeam]SPSLondon"            
+            Ensure             = "Present"
+        }
+
+	    TeamsChannel Sessions
+        {
+            TeamName           = "SPS London"
+            DisplayName        = "Sessions"
+            Description        = "Channel to capture information about sessions"
+            GlobalAdminAccount = $Creds
+            DependsOn          = "[TeamsTeam]SPSLondon"            
+            Ensure             = "Present"
+        }
+    }
+}
+
+$configData = @{
+    AllNodes = @(
+        @{
+            NodeName = "localhost"
+            PSDSCAllowPlaintextPassword = $true
+        }
+    )
+}
+Demo3 -ConfigurationData $configData
